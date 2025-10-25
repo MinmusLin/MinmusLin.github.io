bc912dddbebc78b6f6cf396a560652b672409ba6
林继申 <linjishen.dev@bytedance.com>
2025-10-25 15:29:03 +0800

    add

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,34 +1,34 @@
 <template>
-  <Layout/>
+  <Layout>
+    <template #doc-before>
+      <Breadcrumb/>
+    </template>
+  </Layout>
 </template>
 
 <script setup lang='ts'>
 import {useData} from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import {nextTick, provide} from 'vue'
+import Breadcrumb from './components/Breadcrumb.vue'
 
 const {isDark} = useData()
 
-const enableTransitions = () =>
-  'startViewTransition' in document &&
-  window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+const enableTransitions = () => 'startViewTransition' in document && window.matchMedia('(prefers-reduced-motion: no-preference)').matches
 
 provide('toggle-appearance', async ({clientX: x, clientY: y}: MouseEvent) => {
   if (!enableTransitions()) {
     isDark.value = !isDark.value
     return
   }
-
   const clipPath = [
     `circle(0px at ${x}px ${y}px)`,
     `circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`
   ]
-
   await document.startViewTransition(async () => {
     isDark.value = !isDark.value
     await nextTick()
   }).ready
-
   document.documentElement.animate(
     {
       clipPath: isDark.value ? clipPath.reverse() : clipPath
